9c8abbad611f9b16347a8fd33889ec0bc0043126
ShundaZhang <zhangshunda@gmail.com>
2023-03-19 23:48:44 +0800

    Adding cavesystem...

---
+++ CyberApocalypse2023/re/rev_cavesystem/dec.py
@@ -0,0 +1,302 @@
+'''
+  if (((((((iVar1 == 0) && ((byte)(local_78._5_1_ * (char)local_58) == '\x14')) &&
+         ((byte)((byte)local_68 - local_68._4_1_) == -6)) &&
+        (((((((byte)(local_68._5_1_ - local_70._2_1_) == -0x2a &&
+             ((byte)((byte)local_78 - (char)local_58) == '\b')) &&
+            (((char)(local_58._7_1_ - (char)local_80) == -0x2b &&
+             (((byte)(local_70._2_1_ * local_88._7_1_) == -0x13 &&
+              ((char)(local_88._4_1_ * (char)local_70) == -0x38)))))) &&
+           ((local_68._2_1_ ^ local_70._4_1_) == 0x55)) &&
+          (((((byte)(local_70._6_1_ - local_58._7_1_) == '4' &&
+             ((byte)(local_50._3_1_ + local_58._2_1_) == -0x71)) &&
+            ((byte)(local_60._4_1_ + local_70._3_1_) == -0x2a)) &&
+           (((local_78._1_1_ ^ local_80._6_1_) == 0x31 &&
+            ((byte)((byte)local_50 * local_78._4_1_) == -0x54)))))) &&
+         (((((byte)(local_50._2_1_ - local_70._2_1_) == -0x3e &&
+            (((local_70._2_1_ ^ local_88._6_1_) == 0x2f &&
+             ((local_80._6_1_ ^ local_68._7_1_) == 0x5a)))) &&
+           ((local_60._4_1_ ^ local_68._7_1_) == 0x40)) &&
+          ((((((byte)local_60 == local_70._2_1_ &&
+              ((byte)(local_78._7_1_ + local_58._1_1_) == -0x68)) &&
+             ((byte)(local_78._7_1_ * local_50._3_1_) == 'h')) &&
+            (((byte)(local_88._1_1_ - local_70._4_1_) == -0x25 &&
+             ((byte)((char)local_70 - local_70._5_1_) == -0x2e)))) &&
+           (((char)(local_68._6_1_ - (char)local_70) == '.' &&
+            ((((byte)local_68 ^ local_78._6_1_) == 0x1a &&
+             ((byte)(local_60._4_1_ * local_88._4_1_) == -0x60)))))))))))) &&
+       ((((((byte)(local_68._6_1_ * local_70._3_1_) == '^' &&
+           ((((byte)(local_80._7_1_ - (byte)local_60) == -0x38 &&
+             ((local_58._1_1_ ^ local_58._5_1_) == 0x56)) &&
+            ((local_70._2_1_ ^ local_60._5_1_) == 0x2b)))) &&
+          ((((((local_58._6_1_ ^ local_80._1_1_) == 0x19 &&
+              ((byte)(local_70._4_1_ - local_60._7_1_) == '\x1a')) &&
+             (((byte)(local_58._2_1_ + local_78._3_1_) == -0x5f &&
+              (((byte)(local_68._5_1_ + local_50._1_1_) == 'V' &&
+               ((local_70._5_1_ ^ local_78._2_1_) == 0x38)))))) &&
+            ((local_60._4_1_ ^ local_50._4_1_) == 9)) &&
+           ((((((char)(local_80._7_1_ * local_68._6_1_) == 'y' &&
+               ((local_68._5_1_ ^ local_70._6_1_) == 0x5d)) &&
+              ((byte)(local_88._2_1_ * (byte)local_68) == '\\')) &&
+             (((byte)(local_80._2_1_ * local_78._2_1_) == '9' && (local_70._5_1_ == local_78._5_ 1_))
+             )) && (((byte)(local_68._3_1_ * local_78._5_1_) == '/' &&
+                    (((byte)((char)local_80 * local_68._5_1_) == -0x55 &&
+                     ((byte)(local_68._7_1_ + local_70._2_1_) == -0x6d)))))))))) &&
+         (((((((local_70._2_1_ ^ local_68._2_1_) == 0x73 &&
+              ((((local_78._4_1_ ^ local_70._7_1_) == 0x40 &&
+                ((byte)(local_70._1_1_ + (byte)local_78) == -0x57)) &&
+               ((local_68._7_1_ ^ local_50._3_1_) == 0x15)))) &&
+             ((((byte)((byte)local_88 + local_50._3_1_) == 'i' &&
+               ((byte)(local_68._2_1_ + local_60._6_1_) == -0x5b)) &&
+              (((local_70._6_1_ ^ local_58._4_1_) == 0x37 &&
+               (((byte)((byte)local_88 * local_70._4_1_) == '\b' &&
+                ((byte)(local_68._2_1_ - (byte)local_50) == -0x3b)))))))) &&
+            ((byte)(local_78._2_1_ + local_50._4_1_) == -0x1c)) &&
+           (((((local_68._3_1_ ^ (byte)local_60) == 0x6e &&
+              ((byte)((byte)local_50 * (byte)local_78) == -0x54)) &&
+             ((byte)(local_58._6_1_ - local_60._7_1_) == '\r')) &&
+            ((((byte)(local_70._6_1_ + local_58._7_1_) == -100 &&
+              ((byte)(local_88._6_1_ + local_68._1_1_) == -0x2c)) &&
+             (((byte)(local_88._7_1_ * local_70._5_1_) == -0x13 &&
+              ((((byte)local_50 ^ local_70._5_1_) == 0x38 &&
+               ((byte)(local_88._1_1_ * local_68._5_1_) == 'd')))))))))) &&
+          ((((byte)local_50 ^ local_50._2_1_) == 0x46 &&
+           (((((((char)(local_88._2_1_ * local_78._3_1_) == '&' &&
+                ((local_70._2_1_ ^ local_78._6_1_) == 0x2b)) &&
+               ((byte)(local_88._1_1_ + local_88._7_1_) == -0x79)) &&
+              (((local_70._3_1_ ^ (byte)local_88) == 0x2a &&
+               ((byte)(local_78._5_1_ - local_88._1_1_) == '\v')))) &&
+             ((byte)(local_70._3_1_ + local_58._6_1_) == -0x32)) &&
+            (((local_78._1_1_ ^ local_80._5_1_) == 0x3b &&
+             ((byte)(local_78._3_1_ - local_50._2_1_) == '\x12')))))))))) &&
+        ((((local_78._1_1_ == local_80._2_1_ &&
+           ((((byte)(local_80._6_1_ - local_50._2_1_) == 'M' &&
+             ((byte)(local_60._2_1_ * local_58._4_1_) == 'N')) && (local_58._2_1_ == (byte)local_ 68)
+            ))) && (((local_60._7_1_ ^ local_58._3_1_) == 0x38 &&
+                    ((char)(local_68._6_1_ + local_70._1_1_) == -0x6c)))) &&
+         ((byte)(local_60._1_1_ + local_58._4_1_) == -0x31)))))) &&
+      ((((local_60._4_1_ == local_78._4_1_ && ((char)(local_80._4_1_ + local_70._1_1_) == 'f'))  &&
+        (((byte)(local_50._4_1_ + local_68._4_1_) == -0xf &&
+         ((((byte)(local_60._1_1_ - local_78._5_1_) == '\x11' &&
+           ((byte)(local_68._4_1_ - local_58._1_1_) == 'D')) &&
+          ((byte)(local_80._1_1_ - local_68._3_1_) == 'D')))))) &&
+       ((((local_58._5_1_ ^ local_58._3_1_) == 1 && ((local_68._2_1_ ^ local_50._1_1_) == 0xd))  &&
+        ((((byte)(local_80._3_1_ - local_70._4_1_) == -0x15 &&
+          (((((char)(local_78._7_1_ + (char)local_70) == -0x67 &&
+             ((byte)((char)local_70 + local_80._5_1_) == -0x6b)) &&
+            (((byte)(local_80._4_1_ - (byte)local_88) == -0x17 &&
+             (((((byte)(local_68._2_1_ + local_70._7_1_) == '`' &&
+                ((byte)(local_88._5_1_ + local_58._5_1_) == -0x6a)) &&
+               ((byte)(local_58._1_1_ * local_60._2_1_) == '`')) &&
+              (((byte)((char)local_58 * local_78._5_1_) == '\x14' &&
+               ((byte)(local_70._3_1_ - local_58._4_1_) == '\x03')))))))) &&
+           ((byte)(local_50._1_1_ + local_78._4_1_) == -0x6b)))) &&
+         ((((byte)(local_80._2_1_ * local_58._5_1_) == -0x26 &&
+           ((byte)(local_88._1_1_ + local_60._1_1_) == -0x3c)) &&
+          (((byte)(local_60._7_1_ - local_88._1_1_) == '\v' &&
+           (((local_60._3_1_ == local_78._3_1_ && ((byte)(local_68._7_1_ + local_60._7_1_) == -0 x6d)
+             ) && ((byte)(local_80._4_1_ * local_50._2_1_) == 'Q')))))))))))))) &&
+     (((((byte)((char)local_80 * local_70._2_1_) == 'A' &&
+        ((byte)(local_60._6_1_ - local_70._7_1_) == 'E')) &&
+       ((byte)(local_88._7_1_ + local_68._5_1_) == 'h')) &&
+      (((((char)(local_68._4_1_ + local_88._4_1_) == -0x44 &&
+         ((byte)(local_70._7_1_ + (byte)local_68) == -0x5e)) &&
+        (((char)(local_70._1_1_ + local_88._5_1_) == 'e' &&
+         ((((byte)(local_60._3_1_ * local_70._5_1_) == -0x13 &&
+           ((local_80._5_1_ ^ local_60._5_1_) == 0x10)) &&
+          ((char)((char)local_58 - local_80._4_1_) == ';')))))) &&
+       (((((char)(local_78._7_1_ - (char)local_80) == '\t' &&
+          ((local_88._7_1_ ^ local_60._2_1_) == 0x41)) &&
+         ((char)(local_88._5_1_ - local_60._3_1_) == -3)) &&
+        (((((local_50._4_1_ ^ local_78._2_1_) == 0x1a && ((local_88._1_1_ ^ local_88._3_1_) == 0 x2f)
+           ) && (((byte)(local_78._1_1_ - local_68._7_1_) == '+' &&
+                 (((((byte)((char)local_80 + local_78._4_1_) == -0x2d &&
+                    ((byte)(local_80._3_1_ * local_58._5_1_) == -0x28)) &&
+                   ((byte)(local_70._3_1_ + local_88._6_1_) == -0x2e)) &&
+                  (((byte)(local_88._5_1_ + local_88._3_1_) == -0x55 &&
+                   ((byte)(local_68._3_1_ - local_60._7_1_) == -0x2e)))))))) &&
+         (((byte)local_78 ^ local_68._1_1_) == 0x10))))))))))
+'''
+
+from z3 import *
+
+local_88 = [ BitVec('local_88_%s' % i, 8) for i in range(8) ]
+local_80 = [ BitVec('local_80_%s' % i, 8) for i in range(8) ]
+local_78 = [ BitVec('local_78_%s' % i, 8) for i in range(8) ]
+local_70 = [ BitVec('local_70_%s' % i, 8) for i in range(8) ] 
+local_68 = [ BitVec('local_68_%s' % i, 8) for i in range(8) ]  
+local_60 = [ BitVec('local_60_%s' % i, 8) for i in range(8) ] 
+local_58 = [ BitVec('local_58_%s' % i, 8) for i in range(8) ] 
+local_50 = [ BitVec('local_50_%s' % i, 8) for i in range(8) ] 
+local_48 = [ BitVec('local_48_%s' % i, 8) for i in range(8) ] 
+local_40 = [ BitVec('local_40_%s' % i, 8) for i in range(8) ] 
+local_38 = [ BitVec('local_38_%s' % i, 8) for i in range(8) ] 
+local_30 = [ BitVec('local_30_%s' % i, 8) for i in range(8) ] 
+local_28 = [ BitVec('local_28_%s' % i, 8) for i in range(8) ] 
+local_20 = [ BitVec('local_20_%s' % i, 8) for i in range(8) ] 
+local_18 = [ BitVec('local_18_%s' % i, 8) for i in range(8) ] 
+local_10 = [ BitVec('local_10_%s' % i, 8) for i in range(8) ] 
+
+s = Solver()
+
+for i in range(8):
+	s.add(local_88[i] >= 0x20)
+	s.add(local_88[i] < 0x7f)
+	s.add(local_80[i] >= 0x20)
+	s.add(local_80[i] < 0x7f)
+	s.add(local_78[i] >= 0x20)
+	s.add(local_78[i] < 0x7f)
+	s.add(local_70[i] >= 0x20)
+	s.add(local_70[i] < 0x7f)
+	s.add(local_68[i] >= 0x20)
+	s.add(local_68[i] < 0x7f)
+	s.add(local_60[i] >= 0x20)
+	s.add(local_60[i] < 0x7f)
+	s.add(local_58[i] >= 0x20)
+	s.add(local_58[i] < 0x7f)
+	s.add(local_50[i] >= 0x20)
+	s.add(local_50[i] < 0x7f)
+	s.add(local_48[i] >= 0x20)
+	s.add(local_48[i] < 0x7f)
+	s.add(local_40[i] >= 0x20)
+	s.add(local_40[i] < 0x7f)
+	s.add(local_38[i] >= 0x20)
+	s.add(local_38[i] < 0x7f)
+	s.add(local_30[i] >= 0x20)
+	s.add(local_30[i] < 0x7f)
+	s.add(local_28[i] >= 0x20)
+	s.add(local_28[i] < 0x7f)
+	s.add(local_20[i] >= 0x20)
+	s.add(local_20[i] < 0x7f)
+	s.add(local_18[i] >= 0x20)
+	s.add(local_18[i] < 0x7f)
+	s.add(local_10[i] >= 0x20)
+	s.add(local_10[i] < 0x7f)
+
+
+s.add((local_78[5]* local_58[0])%256 == (ord('\x14') )%256)
+s.add((local_68[0]- local_68[4])%256 == (-6 )%256)
+s.add((local_68[5]- local_70[2])%256 == (-0x2a )%256)
+s.add((local_78[0]- local_58[0])%256 == (ord('\b') )%256)
+s.add((local_58[7]- local_80[0])%256 == (-0x2b )%256)
+s.add((local_70[2]* local_88[7])%256 == (-0x13%256 )%256)
+s.add((local_88[4]* local_70[0])%256 == (-0x38 )%256)
+s.add((local_68[2]^ local_70[4])%256 == (0x55 )%256)
+s.add((local_70[6]- local_58[7])%256 == (ord('4') )%256)
+s.add((local_50[3]+ local_58[2])%256 == (-0x71 )%256)
+s.add((local_60[4]+ local_70[3])%256 == (-0x2a )%256)
+s.add((local_78[1]^ local_80[6])%256 == (0x31 )%256)
+s.add((local_50[0]* local_78[4])%256 == (-0x54 )%256)
+s.add((local_50[2]- local_70[2])%256 == (-0x3e )%256)
+s.add((local_70[2]^ local_88[6])%256 == (0x2f )%256)
+s.add((local_80[6]^ local_68[7])%256 == (0x5a )%256)
+s.add((local_60[4]^ local_68[7])%256 == (0x40 )%256)
+s.add((local_60[0])%256 == (local_70[2])%256)
+s.add((local_78[7]+ local_58[1])%256 == (-0x68 )%256)
+s.add((local_78[7]* local_50[3])%256 == (ord('h') )%256)
+s.add((local_88[1]- local_70[4])%256 == (-0x25 )%256)
+s.add((local_70[0]- local_70[5])%256 == (-0x2e )%256)
+s.add((local_68[6]- local_70[0])%256 == (ord('.') )%256)
+s.add((local_68[0]^ local_78[6])%256 == (0x1a )%256)
+s.add((local_60[4]* local_88[4])%256 == (-0x60 )%256)
+s.add((local_68[6]* local_70[3])%256 == (ord('^') )%256)
+s.add((local_80[7]- local_60[0])%256 == (-0x38 )%256)
+s.add((local_58[1]^ local_58[5])%256 == (0x56 )%256)
+s.add((local_70[2]^ local_60[5])%256 == (0x2b )%256)
+s.add((local_58[6]^ local_80[1])%256 == (0x19 )%256)
+s.add((local_70[4]- local_60[7])%256 == (ord('\x1a') )%256)
+s.add((local_58[2]+ local_78[3])%256 == (-0x5f )%256)
+s.add((local_68[5]+ local_50[1])%256 == (ord('V') )%256)
+s.add((local_70[5]^ local_78[2])%256 == (0x38 )%256)
+s.add((local_60[4]^ local_50[4])%256 == (9 )%256)
+s.add((local_80[7]* local_68[6])%256 == (ord('y') )%256)
+s.add((local_68[5]^ local_70[6])%256 == (0x5d )%256)
+s.add((local_88[2]* local_68[0])%256 == (ord('\\') )%256)
+s.add((local_80[2]* local_78[2])%256 == (ord('9') )%256)
+s.add((local_70[5])%256 == (local_78[5])%256)
+s.add((local_68[3]* local_78[5])%256 == (ord('/') )%256)
+s.add((local_80[0]* local_68[5])%256 == (-0x55 )%256)
+s.add((local_68[7]+ local_70[2])%256 == (-0x6d )%256)
+s.add((local_70[2]^ local_68[2])%256 == (0x73 )%256)
+s.add((local_78[4]^ local_70[7])%256 == (0x40 )%256)
+s.add((local_70[1]+ local_78[0])%256 == (-0x57 )%256)
+s.add((local_68[7]^ local_50[3])%256 == (0x15 )%256)
+s.add((local_88[0]+ local_50[3])%256 == (ord('i') )%256)
+s.add((local_68[2]+ local_60[6])%256 == (-0x5b )%256)
+s.add((local_70[6]^ local_58[4])%256 == (0x37 )%256)
+s.add((local_88[0]* local_70[4])%256 == (ord('\b') )%256)
+s.add((local_68[2]- local_50[0])%256 == (-0x3b )%256)
+s.add((local_78[2]+ local_50[4])%256 == (-0x1c )%256)
+s.add((local_68[3]^ local_60[0])%256 == (0x6e )%256)
+s.add((local_50[0]* local_78[0])%256 == (-0x54 )%256)
+s.add((local_58[6]- local_60[7])%256 == (ord('\r') )%256)
+s.add((local_70[6]+ local_58[7])%256 == (-100 )%256)
+s.add((local_88[6]+ local_68[1])%256 == (-0x2c )%256)
+s.add((local_88[7]* local_70[5])%256 == (-0x13 )%256)
+s.add((local_50[0]^ local_70[5])%256 == (0x38 )%256)
+s.add((local_88[1]* local_68[5])%256 == (ord('d') )%256)
+s.add((local_50[0]^ local_50[2])%256 == (0x46 )%256)
+s.add((local_88[2]* local_78[3])%256 == (ord('&') )%256)
+s.add((local_70[2]^ local_78[6])%256 == (0x2b )%256)
+s.add((local_88[1]+ local_88[7])%256 == (-0x79 )%256)
+s.add((local_70[3]^ local_88[0])%256 == (0x2a )%256)
+s.add((local_78[5]- local_88[1])%256 == (ord('\v') )%256)
+s.add((local_70[3]+ local_58[6])%256 == (-0x32 )%256)
+s.add((local_78[1]^ local_80[5])%256 == (0x3b )%256)
+s.add((local_78[3]- local_50[2])%256 == (ord('\x12') )%256)
+s.add((local_78[1])%256 == (local_80[2])%256)
+s.add((local_80[6]- local_50[2])%256 == (ord('M') )%256)
+s.add((local_60[2]* local_58[4])%256 == (ord('N') )%256)
+s.add((local_58[2])%256 == (local_68[0])%256)
+s.add((local_60[7]^ local_58[3])%256 == (0x38 )%256)
+s.add((local_68[6]+ local_70[1])%256 == (-0x6c )%256)
+s.add((local_60[1]+ local_58[4])%256 == (-0x31 )%256)
+s.add((local_60[4])%256 == (local_78[4])%256)
+s.add((local_80[4]+ local_70[1])%256 == (ord('f')  )%256)
+s.add((local_50[4]+ local_68[4])%256 == (-0xf )%256)
+s.add((local_60[1]- local_78[5])%256 == (ord('\x11') )%256)
+s.add((local_68[4]- local_58[1])%256 == (ord('D') )%256)
+s.add((local_80[1]- local_68[3])%256 == (ord('D') )%256)
+s.add((local_58[5]^ local_58[3])%256 == (1 )%256)
+s.add((local_68[2]^ local_50[1])%256 == (0xd  )%256)
+s.add((local_80[3]- local_70[4])%256 == (-0x15 )%256)
+s.add((local_78[7]+ local_70[0])%256 == (-0x67 )%256)
+s.add((local_70[0]+ local_80[5])%256 == (-0x6b )%256)
+s.add((local_80[4]- local_88[0])%256 == (-0x17 )%256)
+s.add((local_68[2]+ local_70[7])%256 == (ord('`') )%256)
+s.add((local_88[5]+ local_58[5])%256 == (-0x6a )%256)
+s.add((local_58[1]* local_60[2])%256 == (ord('`') )%256)
+s.add((local_58[0]* local_78[5])%256 == (ord('\x14') )%256)
+s.add((local_70[3]- local_58[4])%256 == (ord('\x03') )%256)
+s.add((local_50[1]+ local_78[4])%256 == (-0x6b )%256)
+s.add((local_80[2]* local_58[5])%256 == (-0x26 )%256)
+s.add((local_88[1]+ local_60[1])%256 == (-0x3c )%256)
+s.add((local_60[7]- local_88[1])%256 == (ord('\v') )%256)
+s.add((local_60[3])%256 == (local_78[3])%256)
+s.add((local_68[7]+ local_60[7])%256 == (-0x6d)%256)
+s.add((local_80[4]* local_50[2])%256 == (ord('Q') )%256)
+s.add((local_80[0]* local_70[2])%256 == (ord('A') )%256)
+s.add((local_60[6]- local_70[7])%256 == (ord('E') )%256)
+s.add((local_88[7]+ local_68[5])%256 == (ord('h') )%256)
+s.add((local_68[4]+ local_88[4])%256 == (-0x44 )%256)
+s.add((local_70[7]+ local_68[0])%256 == (-0x5e )%256)
+s.add((local_70[1]+ local_88[5])%256 == (ord('e') )%256)
+s.add((local_60[3]* local_70[5])%256 == (-0x13 )%256)
+s.add((local_80[5]^ local_60[5])%256 == (0x10 )%256)
+s.add((local_58[0]- local_80[4])%256 == (ord(';') )%256)
+s.add((local_78[7]- local_80[0])%256 == (ord('\t') )%256)
+s.add((local_88[7]^ local_60[2])%256 == (0x41 )%256)
+s.add((local_88[5]- local_60[3])%256 == (-3 )%256)
+s.add((local_50[4]^ local_78[2])%256 == (0x1a )%256)
+s.add((local_88[1]^ local_88[3])%256 == (0x2f)%256)
+s.add((local_78[1]- local_68[7])%256 == (ord('+') )%256)
+s.add((local_80[0]+ local_78[4])%256 == (-0x2d )%256)
+s.add((local_80[3]* local_58[5])%256 == (-0x28 )%256)
+s.add((local_70[3]+ local_88[6])%256 == (-0x2e )%256)
+s.add((local_88[5]+ local_88[3])%256 == (-0x55 )%256)
+s.add((local_68[3]- local_60[7])%256 == (-0x2e )%256)
+s.add((local_78[0]^ local_68[1])%256 == (0x10)%256)
+
+print s.check()
+print s.model()
+
+
