ae10b5e8deec22ad6a74247cc7f88df5cc232439
ShundaZhang <shunda.zhang@intel.com>
2023-03-21 13:20:49 +0800

    Solving Janken, move to london network.

--- CyberApocalypse2023/misc/Janken/misc_janken/dec.py
+++ CyberApocalypse2023/misc/Janken/misc_janken/dec.py
@@ -3,10 +3,10 @@ import ctypes
 
 context.log_level = 'debug'
 
-#ip,port = 
-#io = remote(ip,port)
+ip,port = '104.248.169.177',31603
+io = remote(ip,port)
 
-io = process('./janken')
+#io = process('./janken')
 
 lib = ctypes.CDLL('./librand3.so')
 #nrd = lib.rand3(0)
@@ -17,8 +17,8 @@ play = ["rock", "scissors", "paper"]
 win  = ["paper","rock" , "scissors"]
 i = 0
 for i in range(100):
-	nrd = lib.rand3(0)
 	io.recvuntil(">>")
+	nrd = lib.rand3(0)
 	io.sendline(win[nrd])
 	print i
 
