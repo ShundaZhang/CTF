d454d832fb7f539c5f6b900f96ad3cae8082fd81
szhan21 <shunda.zhang@intel.com>
2023-03-21 13:03:01 +0800

    Solving Janken...

--- CyberApocalypse2023/misc/Janken/misc_janken/dec.py
+++ CyberApocalypse2023/misc/Janken/misc_janken/dec.py
@@ -1,11 +1,24 @@
 from pwn import *
 import ctypes
 
+context.log_level = 'debug'
+
 #ip,port = 
 #io = remote(ip,port)
 
 io = process('./janken')
 
 lib = ctypes.CDLL('./librand3.so')
-nrd = lib.rand3(0)
-print nrd
+#nrd = lib.rand3(0)
+
+io.sendlineafter('>>', '1')
+
+play = ["rock", "scissors", "paper"]
+win  = ["paper","rock" , "scissors"]
+i = 0
+for i in range(100):
+	nrd = lib.rand3(0)
+	io.recvuntil(">>")
+	io.sendline(win[nrd])
+	print i
+
