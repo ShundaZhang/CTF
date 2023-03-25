f581a5e9360bae54d1d0ed704a8cc564458391da
szhan21 <shunda.zhang@intel.com>
2023-03-25 18:54:13 +0800

    Debugging colliding_heritage.

--- CyberApocalypse2023/crypto/crypto_colliding_heritage/dec.py
+++ CyberApocalypse2023/crypto/crypto_colliding_heritage/dec.py
@@ -8,8 +8,12 @@ m1 = 'd131dd02c5e6eec4693d9a0698aff95c2fcab58712467eab4004583eb8fb7f8955ad340609
 m2 = 'd131dd02c5e6eec4693d9a0698aff95c2fcab50712467eab4004583eb8fb7f8955ad340609f4b30283e4888325f1415a085125e8f7cdc99fd91dbdf280373c5bd8823e3156348f5bae6dacd436c919c6dd53e23487da03fd02396306d248cda0e99f33420f577ee8ce54b67080280d1ec69821bcb6a8839396f965ab6ff72a70'.decode('hex')
 
 io.recvuntil(': ')
-print io.recvline()
+g = io.recvline().strip()
 io.recvuntil(': ')
-print io.recvline()
+y = io.recvline().strip()
 io.recvuntil(': ')
-print io.recvline()
+p = io.recvline().strip()
+
+print g
+print y
+print p
