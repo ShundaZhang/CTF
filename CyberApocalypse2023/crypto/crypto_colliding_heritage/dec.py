a3b3a948bcb78cd048381f1b4537a4095a035f94
Zhang, Shunda <zhangshunda@gmail.com>
2023-03-25 12:34:45 +0000

    Solved colliding_heritage.

--- CyberApocalypse2023/crypto/crypto_colliding_heritage/dec.py
+++ CyberApocalypse2023/crypto/crypto_colliding_heritage/dec.py
@@ -21,31 +21,31 @@ def sign(msg, x, g, p, q):
 ip, port = '64.227.41.83', 31866
 io = remote(ip, port)
 
-m1 = 'd131dd02c5e6eec4693d9a0698aff95c2fcab58712467eab4004583eb8fb7f8955ad340609f4b30283e488832571415a085125e8f7cdc99fd91dbdf280373c5bd8823e3156348f5bae6dacd436c919c6dd53e2b487da03fd02396306d248cda0e99f33420f577ee8ce54b67080a80d1ec69821bcb6a8839396f9652b6ff72a70'
+m1 = '4dc968ff0ee35c209572d4777b721587d36fa7b21bdc56b74a3dc0783e7b9518afbfa200a8284bf36e8e4b55b35f427593d849676da0d1555d8360fb5f07fea2'.encode()
 
-m2 = 'd131dd02c5e6eec4693d9a0698aff95c2fcab50712467eab4004583eb8fb7f8955ad340609f4b30283e4888325f1415a085125e8f7cdc99fd91dbdf280373c5bd8823e3156348f5bae6dacd436c919c6dd53e23487da03fd02396306d248cda0e99f33420f577ee8ce54b67080280d1ec69821bcb6a8839396f965ab6ff72a70'
+m2 = '4dc968ff0ee35c209572d4777b721587d36fa7b21bdc56b74a3dc0783e7b9518afbfa202a8284bf36e8e4b55b35f427593d849676da0d1d55d8360fb5f07fea2'.encode()
 
-io.recvuntil(': ')
+io.recvuntil(b': ')
 g = int(io.recvline().strip())
-io.recvuntil(': ')
+io.recvuntil(b': ')
 y = int(io.recvline().strip())
-io.recvuntil(': ')
+io.recvuntil(b': ')
 p = int(io.recvline().strip())
 q = (p - 1)//2
 #print(g)
 #print(y)
 #print(p)
 
-io.sendlineafter('>', 'S')
-io.sendlineafter('>', m1)
+io.sendlineafter(b'>', b'S')
+io.sendlineafter(b'>', m1)
 buf = io.recvline().split()
 s1 = int(buf[1][1:-1])
 e1 = int(buf[2][:-1])
 #print(buf)
 #print(s1)
 #print(e1)
-io.sendlineafter('>', 'S')
-io.sendlineafter('>', m2)
+io.sendlineafter(b'>', b'S')
+io.sendlineafter(b'>', m2)
 buf = io.recvline().split()
 s2 = int(buf[1][1:-1])
 e2 = int(buf[2][:-1])
@@ -71,9 +71,9 @@ s, e = sign(msg, x, g, p, q)
 #print(hex(s))
 #print(hex(e))
 
-io.sendlineafter('>', 'V')
-io.sendlineafter('>', msg.hex())
-io.sendlineafter('>', hex(s)[2:])
-io.sendlineafter('>', hex(e)[2:])
+io.sendlineafter(b'>', b'V')
+io.sendlineafter(b'>', msg.hex().encode())
+io.sendlineafter(b'>', str(s).encode())
+io.sendlineafter(b'>', str(e).encode())
 
 print(io.recvall())
