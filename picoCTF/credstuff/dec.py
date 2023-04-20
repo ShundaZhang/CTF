273700bfbc92498dcf6302cb2aa772b42ca7ae8d
szhan21 <shunda.zhang@intel.com>
2023-04-20 09:11:41 +0800

    picoctf credstuff

---
+++ picoCTF/credstuff/dec.py
@@ -0,0 +1,12 @@
+'''
+grep cultiris usernames.txt -n
+378:cultiris
+
+cvpbPGS{P7e1S_54I35_71Z3}
+picoCTF{C7r1F_54V35_71M3}
+
+'''
+
+print chr(ord('A')+(ord('G')-ord('A')+13)%26)
+print chr(ord('a')+(ord('e')-ord('a')+13)%26)
+print chr(ord('A')+(ord('Z')-ord('A')+13)%26)
