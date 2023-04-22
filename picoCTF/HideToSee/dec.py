39cebf189d8fde1371ba418b88e370178f075a7c
szhan21 <shunda.zhang@intel.com>
2023-04-22 22:12:10 +0800

    hidetosee

---
+++ picoCTF/HideToSee/dec.py
@@ -0,0 +1,11 @@
+'''
+stegoveritas atbash.jpg
+
+cat steghide_4faf8f973f5a6daf0e0b9573b9371967.bin
+krxlXGU{zgyzhs_xizxp_1u84w779}
+
+sudo eog atbash.jpg
+Replace the letters according to the picture
+
+picoCTF{atbash_crack_1f84d779}
+'''
