36d319e67fe9d998cd4c52690cc466c891af998c
ShundaZhang <zhangshunda@gmail.com>
2023-05-02 22:54:30 +0800

    Solving guess

--- monthly_security_challenge/guess/dec.py
+++ monthly_security_challenge/guess/dec.py
@@ -1,5 +1,6 @@
 from pwn import *
-
+import os
+'''
 context.log_level = 'debug'
 
 io = process('./guess2')
@@ -13,3 +14,18 @@ for i in range(20):
 		break 
 	else:
 		print(i)
+
+'''
+i = 0
+while i < 2**32+1: 
+	with open('log.txt', 'wb') as f:
+		f.write(str(i))
+	buf = os.popen('./guess2 < log.txt').read()
+	print i, buf
+	if 'Nice!' in buf:
+                print('Found!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+                print(i)
+                break
+	i +=1 
+
+
