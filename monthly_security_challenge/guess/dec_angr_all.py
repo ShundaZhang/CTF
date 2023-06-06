525eb50491f6e697e1a67ba6e62c572d50e1255e
ShundaZhang <zhangshunda@gmail.com>
2023-06-06 21:13:43 +0800

    Add angr version of solving all answers of guess.

---
+++ monthly_security_challenge/guess/dec_angr_all.py
@@ -0,0 +1,30 @@
+import angr
+import sys
+
+def main(argv):
+  path_to_binary = argv[1]  # :string
+  project = angr.Project(path_to_binary)
+
+  # Start in main()
+  initial_state = project.factory.entry_state()
+  # Start simulation
+  simulation = project.factory.simgr(initial_state)
+
+  # Find the way yo reach the good address
+  good_address = 0x4006d5
+  
+  # Avoiding this address
+  #avoid_address = 0x080485A8
+  simulation.explore(find=good_address)
+
+  # If found a way to reach the address
+  if simulation.found:
+    solution_state = simulation.found[0]
+
+    # Print the string that Angr wrote to stdin to follow solution_state
+    print(solution_state.posix.dumps(sys.stdin.fileno()))
+  else:
+    raise Exception('Could not find the solution')
+
+if __name__ == '__main__':
+  main(sys.argv)
