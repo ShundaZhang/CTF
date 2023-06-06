ef19e152bff97a4c6194fa08e690bc22acde57a4
szhan21 <shunda.zhang@intel.com>
2023-06-06 10:05:57 +0800

    There are 9 correct answers for guess!

--- monthly_security_challenge/guess/dec.py
+++ monthly_security_challenge/guess/dec.py
@@ -31,3 +31,36 @@ while s.check()==sat:
 
 #Please enter the PIN Number: 2518422944
 #Nice! The real flag is : FLAG{4A_Fl4g_L3t5_D0_Th3_M4th}. Seriously, that is the flag. Congrats!
+
+'''
+sat
+[x = 3447158698]
+[x = 660951436]
+[x = 196583559]
+[x = 2054055067]
+[x = 3911526575]
+[x = 1125319313]
+[x = 2518422944]
+[x = 2982790821]
+[x = 1589687190]
+
+Please enter the PIN Number: 2518422944
+Nice! The real flag is : FLAG{4A_Fl4g_L3t5_D0_Th3_M4th}. Seriously, that is the flag. Congrats!
+Please enter the PIN Number: 196583559
+Nice! The real flag is : FLAG{4A_Fl4g_L3t5_D0_Th3_M4th}. Seriously, that is the flag. Congrats!
+Please enter the PIN Number: 660951436
+Nice! The real flag is : FLAG{G00d_Lu<k_4_F1nDing_A_Fl4g}. Seriously, that is the flag. Congrats!
+Please enter the PIN Number: 2982790821
+Nice! The real flag is : FLAG{G00d_Lu<k_4_F1nDing_A_Fl4g}. Seriously, that is the flag. Congrats!
+Please enter the PIN Number: 3447158698
+Nice! The real flag is : FLAG{Sk1lleD_Pl4y3r_1s_a_H4cker}. Seriously, that is the flag. Congrats!
+Please enter the PIN Number: 3911526575
+Nice! The real flag is : FLAG{Brut3_F0rc3_on3w4y_CrEativity_0Th3r}. Seriously, that is the flag. Congrats!
+Please enter the PIN Number: 1125319313
+Nice! The real flag is : FLAG{Sk1lleD_Pl4y3r_1s_a_H4cker}. Seriously, that is the flag. Congrats!
+Please enter the PIN Number: 1589687190
+Nice! The real flag is : FLAG{Brut3_F0rc3_on3w4y_CrEativity_0Th3r}. Seriously, that is the flag. Congrats!
+Please enter the PIN Number: 2054055067
+Nice! The real flag is : FLAG{R3vErs1ng_1s_4LWays_G00D}. Seriously, that is the flag. Congrats!
+
+'''
