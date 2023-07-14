13b4ff3eba34845968a7a988ed860cfb70cdb64c
ShundaZhang <zhangshunda@gmail.com>
2023-07-14 22:50:45 +0800

    Solved blockchain_paid_contr-actor!

--- BusinessCTF2023/blockchain_paid_contr-actor/dec.py
+++ BusinessCTF2023/blockchain_paid_contr-actor/dec.py
@@ -1,68 +1,21 @@
+#https://ctftime.org/writeup/36492
+
 from web3 import Web3
-import json
+from solcx import compile_source, install_solc
 
-Private key     :  '0x5e7997d7fdeb7ac59215c7619d37c5c522da69739725582b3ef29a040f725aa9'
-Address         :  '0x54C23bB8Cdc454D7cF13739404e8FCB6f752F333'
-Target contract :  '0x0E6BdcF0f33B74b537f8160eab47D32E44534Ab6'
-Setup contract  :  '0x079E941B7287CB55493ECEC66aA451f60cB9DdEB'
+# installs solc compiler, necessary only for first execution
+install_solc("0.8.18")
 
-w3 = Web3(Web3.HTTPProvider('http://94.237.59.199:56351'))
-block_number = w3.eth.block_number
-print(block_number)
+# compiles source code to get the contract abi
+with open("Contract.sol", "rt") as fp:
+    src = fp.read()
+compiled = compile_source(src, output_values=["abi"])
+contract_id, contract_interface = compiled.popitem()
+abi = contract_interface['abi']
 
-account_address = Address
-balance = w3.eth.get_balance(account_address)
-print(balance)
+# contract adress (given in the tcp interface)
+target = '0x8b49D3C4B58cf8fB71e2Ec3B04C70560Ed8bB8E3'
 
-#solcjs --abi --bin Setup.sol #generate .abi .bin
-#with open('Setup_sol_Setup.abi','r') as f:
-#	abi = json.load(f)
-
-#contract_instance = w3.eth.contract(address=SetupContract, abi=abi)
-
-account_from = {
-	'private_key': PrivateKey,
-	'address': account_address,
-}
-
-#construct_txn = contract_instance.constructor().buildTransaction(
-#	{
-#		'from': account_from['address'],
-#		'nonce': w3.eth.get_transaction_count(account_from['address']),
-#	}
-#)
-
-#tx_create = w3.eth.account.sign_transaction(construct_txn, account_from['private_key'])
-#tx_hash = w3.eth.send_raw_transaction(tx_create.rawTransaction)
-#tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-
-with open('Unknown_sol_Unknown.abi','r') as f:
-	abi = json.load(f)
-contract_instance2 = w3.eth.contract(address=TargetContract, abi=abi)
-construct_txn = contract_instance2.functions.updateSensors(10).build_transaction(
-        {
-                'from': account_from['address'],
-                'nonce': w3.eth.get_transaction_count(account_from['address']),
-		'chainId': w3.eth.chain_id,
-		#"gasPrice": w3.to_wei(50, 'gwei'),
-		#"gas": 21000,
-		#"value": w3.to_wei("0", "ether"),
-        }
-)
-tx_create = w3.eth.account.sign_transaction(construct_txn, account_from['private_key'])
-tx_hash = w3.eth.send_raw_transaction(tx_create.rawTransaction)
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-print(f'Tx successful with hash: { tx_receipt.transactionHash.hex() }')
-
-updated = contract_instance2.functions.updated().call()
-print('updated:', updated)
-
-with open('Setup_sol_Setup.abi','r') as f:
-        abi = json.load(f)
-
-contract_instance = w3.eth.contract(address=SetupContract, abi=abi)
-number = contract_instance.functions.isSolved().call()
-
-print(f'The current number stored is: { number } ')
-
-#FLAG=HTB{9P5_50FtW4R3_UPd4t3D}
+w3 = Web3(Web3.HTTPProvider("http://94.237.59.199:56351")) # blockchain gateway from tcp interface
+contract = w3.eth.contract(address=target, abi=abi)
+contract.functions.signContract(1337).transact() # calls update sensor with value of 10
