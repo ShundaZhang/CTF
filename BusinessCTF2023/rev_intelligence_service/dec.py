be2e368c562ee757da95cc52d2a208b84e1874c0
ShundaZhang <zhangshunda@gmail.com>
2023-07-15 12:14:10 +0800

    Solved rev_intelligence_service!

--- BusinessCTF2023/rev_intelligence_service/dec.py
+++ BusinessCTF2023/rev_intelligence_service/dec.py
@@ -25,6 +25,14 @@
       (((int)*(char *)(param_1 + 0x24) + *(char *)(param_1 + 0xe) + 0x79 == 0x108 &&
        (*(char *)(param_1 + 0x27) * 3 + (int)*(char *)(param_1 + 0x11) == 0x1a6)))))) {
 
+  if ((((*(char *)(*(long *)(unaff_RBP + -0x28) + 0x15) * 2 +
+         (int)*(char *)(*(long *)(unaff_RBP + -0x28) + 0x1d) != 0x13c) ||
+       (*(char *)(*(long *)(unaff_RBP + -0x28) + 9) != '7')) ||
+      ((int)*(char *)(*(long *)(unaff_RBP + -0x28) + 0x25) !=
+       *(char *)(*(long *)(unaff_RBP + -0x28) + 0x1d) + 5)) ||
+     (*(char *)(*(long *)(unaff_RBP + -0x28) + 0x10) * 2 +
+      (int)*(char *)(*(long *)(unaff_RBP + -0x28) + 0x1d) != 0xd2))
+
 '''
 
 from z3 import *
@@ -40,8 +48,6 @@ s.add(param_1[0] == ord('H'))
 s.add(param_1[1] == ord('T'))
 s.add(param_1[2] == ord('B'))
 s.add(param_1[3] == ord('{'))
-s.add(param_1[6] == ord('?'))
-s.add(param_1[0x21] == ord('?'))
 s.add(param_1[0x28] == ord('}'))
 
 s.add(param_1[9] + -0x7a - param_1[0x1b] == -0xb8 )
@@ -70,6 +76,30 @@ s.add(param_1[0x1f] + param_1[0xc] + 0x45 == 0x121 )
 s.add(param_1[0x24] + param_1[0xe] + 0x79 == 0x108 )
 s.add(param_1[0x27] * 3 + param_1[0x11] == 0x1a6 )
 
+s.add(param_1[0x15]*6 == param_1[0x25] + 0x1ef)
+s.add(param_1[0x21] + param_1[0x10] == 0x90)
+s.add(param_1[0x14]*4 + param_1[0xb] == 0x1ad)
+s.add(param_1[0x20] - 0x34 == param_1[8] )
+s.add(param_1[0x12]*7 + param_1[0x18] == 0x3c3)
+s.add(param_1[6]*10 + param_1[0x23] == 0x428)
+s.add(param_1[0x1c] + 0xd == param_1[0x12])
+
+s.add(param_1[0x17] == 0x30)
+s.add(param_1[4] + 0x32 == param_1[0x1a] - 7)
+s.add(param_1[0x22]*2 + param_1[0xa] + 0x53 == 0xf2)
+s.add(param_1[0x26] + param_1[0xf]*5 + 0x14 == 0x276)
+s.add(param_1[5] + param_1[0x27] - 0x28 == 0xa9)
+s.add(param_1[7] + param_1[0x19] == 0xc6)
+s.add(param_1[0xc]*5 + param_1[0x1e] == 0x257)
+s.add(param_1[0x16] == 0x72)
+s.add(param_1[0x1f] + param_1[0xe] + 0x46 == 0x113)
+
 print(s.check())
 print(s.model())
 
+'''
+print(s.check())
+while s.check()==sat:
+        print(s.model())
+        s.add(param_1[0] != s.model()[param_1[0]])
+'''
