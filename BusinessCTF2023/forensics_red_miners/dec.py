cbaa604fcfc4b4b67aa530b8386402a22b885d3b
szhan21 <shunda.zhang@intel.com>
2023-07-17 14:14:45 +0800

    solved red_miners.

---
+++ BusinessCTF2023/forensics_red_miners/dec.py
@@ -0,0 +1,10 @@
+'''
+echo X3QwX200cnN9Cg==|base64 -d
+_t0_m4rs}
+echo cGFydDI9Il90aDMxcl93NHkiCg== |base64 -d
+part2="_th31r_w4y"
+echo -n cGFydDE9IkhUQnttMW4xbmciCg==|base64 -d
+part1="HTB{m1n1ng"
+
+HTB{m1n1ng_th31r_w4y_t0_m4rs}
+'''
