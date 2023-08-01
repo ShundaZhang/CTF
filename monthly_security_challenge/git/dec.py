4081174081f5908a38227445f5aee06d7154e1c3
szhan21 <shunda.zhang@intel.com>
2023-08-01 22:19:13 +0800

    Aug Monthly solved. but only submitted the first one...

---
+++ monthly_security_challenge/git/dec.py
@@ -0,0 +1,15 @@
+'''
+grep secret -r .
+git diff 2 hash
+
+Found:
+FLAG{th4t-w4s-345y}
+FLAG{tH15_Is-7h3_u17iM4T3_g04l}
+
+Ask chatgpt: git all history
+git log --all -p
+
+Found
+FLAG{h3y-7Ha7_1s-mY-5t4Sh}
+
+'''
