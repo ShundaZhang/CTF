6f0598e347a3cc23c21a5b845b06040bc74600f7
szhan21 <shunda.zhang@intel.com>
2024-02-18 16:07:40 +0800

    Solved OSINT 1 in 0xl4ugh!

---
+++ 0xL4ugh/OSINT/LostInHistory/dec.py
@@ -0,0 +1,6 @@
+'''
+Middle east + museum, probably Egypt
+Google image -> https://www.facebook.com/100057620133170/videos/1170538993812034/?__so__=permalink
+
+0xL4ugh{National Museum of Egyptian Civilization}
+'''
