eea5f24d2e78801a850ce7dafdfc733b1094105f
szhan21 <shunda.zhang@intel.com>
2024-02-19 14:36:01 +0800

    Solved Cheater!

---
+++ 0xL4ugh/OSINT/Cheater/dec.py
@@ -0,0 +1,15 @@
+'''
+Our team received a request from a man who believes his wife may be cheating on him. He asked us to help by checking her accounts for any evidence. He provided his wife's name, "Hamdia Eldhkawy" and mentioned that a friend informed him she shared a picture with someone on social media. He couldn't find the image and wants us to discover the man's real name.
+
+https://medium.com/@karimwalid/0xl4ugh-ctf-osint-challenge-cheater-2ae337524032
+
+Archive
+archive.today 
+
+Email, Phone OSINT
+https://epieos.com/
+
+
+0xL4ugh{Abdelfatah_ElCanaway}
+
+'''
