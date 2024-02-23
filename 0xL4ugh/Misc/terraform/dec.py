4f5df3240e4b1a670f437b9a46db43e46e2d5d49
szhan21 <shunda.zhang@intel.com>
2024-02-23 17:12:05 +0800

    Solved terraform!

---
+++ 0xL4ugh/Misc/terraform/dec.py
@@ -0,0 +1,10 @@
+'''
+Terraform
+https://developer.hashicorp.com/terraform/language/functions/strrev
+
+strrev(var.FLAG)
+"}3t4T_0rDnA_3t4Ts_fT{hgu4Lx0"
+
+echo "}3t4T_0rDnA_3t4Ts_fT{hgu4Lx0"|rev
+0xL4ugh{Tf_sT4t3_AnDr0_T4t3}
+'''
