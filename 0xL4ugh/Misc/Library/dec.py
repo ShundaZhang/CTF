703a0149b6a887a1c52c1d4383634a3d49856e29
szhan21 <shunda.zhang@intel.com>
2024-03-07 16:25:27 +0800

    Solved Library!

---
+++ 0xL4ugh/Misc/Library/dec.py
@@ -0,0 +1,32 @@
+'''
+Library Management System
+1. Add Member
+2. Add Book
+3. Display Books
+4. Search Book
+5. Check Out Book
+6. Return Book
+7. Save Book
+8. Check File Presence
+0. Exit
+Enter your choice (0-8): 8
+Enter the name of the book (file) to check: -a
+$FLAG
+'--ignore=[^Z][^-]*'
+.
+..
+0xL4ugh{TrU5t_M3_LiF3_I5_H4rD3r!}
+FLAG
+GOT
+The Tale of Johnothy
+a
+asdsad
+asem
+a||ls
+a||ls;
+challenge.py
+challenge1.py
+exec.sh
+me
+oimate
+'''
