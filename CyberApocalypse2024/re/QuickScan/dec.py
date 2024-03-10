15d7b6a2e5d76485ec97163eb024054dcc375955
szhan21 <shunda.zhang@intel.com>
2024-03-10 13:02:42 +0800

    Solving Quick Scan.

--- CyberApocalypse2024/re/QuickScan/dec.py
+++ CyberApocalypse2024/re/QuickScan/dec.py
@@ -1,8 +1,15 @@
 from pwn import *
+import base64
 
 ip, port = '94.237.63.2', 34408
 io = remote(ip, port)
 
 io.recvuntil('ELF:')
-base64_buf = io.recvline()
+base64_buf = io.recvline().strip().encode()
 print(base64_buf)
+decoded_data = base64.b64decode(base64_buf)
+
+with open('a.out', 'wb') as f:
+	f.write(decoded_data)
+
+
