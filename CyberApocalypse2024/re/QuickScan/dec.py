61502503c7b88cc4aa92f7ac0a790ae907033a82
ShundaZhang <shunda.zhang@intel.com>
2024-03-10 05:37:08 +0000

    Solving Quick Scan.

--- CyberApocalypse2024/re/QuickScan/dec.py
+++ CyberApocalypse2024/re/QuickScan/dec.py
@@ -2,18 +2,21 @@ from pwn import *
 import base64
 import subprocess
 
-ip, port = '94.237.63.2', 34408
+ip, port = '94.237.51.233', 52391
 io = remote(ip, port)
 
-io.recvuntil('ELF:')
-base64_buf = io.recvline().strip().decode()
-#print(base64_buf)
-decoded_data = base64.b64decode(base64_buf)
+for _ in range(129):
+    io.recvuntil('ELF:')
+    base64_buf = io.recvline().strip().decode()
+    #print(base64_buf)
+    decoded_data = base64.b64decode(base64_buf)
 
-with open('a.out', 'wb') as f:
-	f.write(decoded_data)
+    with open('a.out', 'wb') as f:
+        f.write(decoded_data)
 
-proc = subprocess.Popen(["python3", "dec_angr.py"], stdout=subprocess.PIPE)
-buf = proc.stdout.read().strip().split(' ')[-1].split('>')[0]
-print(buf)
-
+    proc = subprocess.Popen(["python3", "dec_angr.py"], stdout=subprocess.PIPE)
+    buf = proc.stdout.read().strip().decode().split(' ')[-1].split('>')[0].split('0x')[-1]
+    print(buf)
+    io.recvuntil('?')
+    io.sendline(buf.encode())
+print(io.recvall())
