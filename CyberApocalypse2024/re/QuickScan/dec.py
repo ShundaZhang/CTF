b01e55c4673e749b7e0feb6c434c335620239497
ShundaZhang <shunda.zhang@intel.com>
2024-03-10 06:41:30 +0000

    Solved Quick Scan.

--- CyberApocalypse2024/re/QuickScan/dec.py
+++ CyberApocalypse2024/re/QuickScan/dec.py
@@ -1,6 +1,7 @@
 from pwn import *
 import base64
-import subprocess
+import angr
+from Crypto.Util.number import long_to_bytes, bytes_to_long
 
 ip, port = '94.237.51.233', 52391
 io = remote(ip, port)
@@ -16,12 +17,26 @@ for _ in range(128):
     #print(base64_buf)
     decoded_data = base64.b64decode(base64_buf)
 
-    with open('a.out', 'wb') as f:
+    with open('./a.out', 'wb') as f:
         f.write(decoded_data)
 
-    proc = subprocess.Popen(["python3", "dec_angr.py"], stdout=subprocess.PIPE)
-    buf = proc.stdout.read().strip().decode().split(' ')[-1].split('>')[0].split('0x')[-1]
-    print(buf)
+    proj = angr.Project("./a.out", auto_load_libs=False)
+    #proj = angr.load_shellcode(decoded_data, 'amd64')
+
+    entry_addr = proj.entry
+    state = proj.factory.entry_state(addr=entry_addr)
+
+    simgr = proj.factory.simgr(state)
+    simgr.step(n=1)
+
+    rsi_addr = simgr.active[0].regs.rsi
+
+    str_content = simgr.active[0].memory.load(rsi_addr-1, 0x18)
+    #print(rsi_addr)
+    #print(str_content)
+
+    buf = long_to_bytes(str_content.v).hex()
     io.recvuntil('?')
-    io.sendline(buf.zfill(48).encode())
+    io.sendline(buf)
 print(io.recvall())
+#HTB{y0u_4n4lyz3d_th3_p4tt3ns!}
