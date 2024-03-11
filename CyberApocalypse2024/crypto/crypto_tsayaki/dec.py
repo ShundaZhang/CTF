4adc96f4a4155154002c6952a92a1c553b1f9b6a
ShundaZhang <shunda.zhang@intel.com>
2024-03-11 02:56:54 +0000

    ctf

---
+++ CyberApocalypse2024/crypto/crypto_tsayaki/dec.py
@@ -0,0 +1,17 @@
+'''
+Here is my special message: d510adb7307777a91f067a7eac91fc10c3bbba35
+Round 1/10
+Enter your target ciphertext (in hex) : d510adb7307777a91f067a7eac91fc10c3bbba35
+[1/1] Enter your encryption key (in hex) : 00000000000000000000000000000000
+Hmm ... close enough, but e8525f1be235cd7030f7f8652ab7f4436974cfd98a7ddf00 does not look like d510adb7307777a91f067a7eac91fc10c3bbba35 at all! Bye...
+Nope.
+
+Here is my special message: 7f23510fd2b101f629079e4d4917b118c82e586f
+Round 1/10
+Enter your target ciphertext (in hex) : 7f23510fd2b101f629079e4d4917b118c82e586f
+[1/1] Enter your encryption key (in hex) : 00000000000000000000000000000000
+Hmm ... close enough, but ca8519bcc81569e6754fff1e88a01c52af54a3ac7cc7b68d does not look like 7f23510fd2b101f629079e4d4917b118c82e586f at all! Bye...
+Nope.
+'''
+
+
