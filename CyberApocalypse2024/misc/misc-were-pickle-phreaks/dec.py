ce929fcacef6aa6c567874db2bd32022cd2f291c
szhan21 <shunda.zhang@intel.com>
2024-03-14 07:57:39 +0800

    ctf

--- CyberApocalypse2024/misc/misc-were-pickle-phreaks/dec.py
+++ CyberApocalypse2024/misc/misc-were-pickle-phreaks/dec.py
@@ -1,3 +1,10 @@
 '''
+https://github.com/mahaloz/ctf-wiki-en/blob/master/docs/pwn/linux/sandbox/python-sandbox-escape.md
+
+().__class__.__bases__[0].__subclasses__()[40](r'C:\1.php').read()
 Phreaks.__class__.__base__.__subclasses__()[84].__subclasses__()[2].__subclasses__()[1]
+
+But it is for python2...
 '''
+
+
