dc076b2af7e65902473b3b857586dbec781d0525
ShundaZhang <shunda.zhang@intel.com>
2024-05-18 14:44:26 +0000

    Solved crypto2!

--- BusinessCTF2024/crypto/crypto_living_with_elegance/dec.py
+++ BusinessCTF2024/crypto/crypto_living_with_elegance/dec.py
@@ -1,13 +1,21 @@
 from pwn import *
+from Crypto.Util.number import long_to_bytes
 
-ip, port = '83.136.252.165', 47472
+ip, port = '94.237.54.178', 54095
 io = remote(ip, port)
-
+flag = ''
 for i in range(471):
-	for _ in range(1):
+	bf = '0'
+	for _ in range(100):
 		io.recvuntil('Specify the index of the bit you want to get an encryption for :')
 		io.sendline(str(i))
 		io.recvline()
 		io.recvline()
 		buf = io.recvline()
-		print(buf)
+		x = int(buf.decode().strip().split(' ')[-1])
+		if x < 0:
+			bf = '1'
+			break
+	flag += bf
+	print(flag)
+print(long_to_bytes(int(flag,2)))
