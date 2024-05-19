358dd974781578b341df1d361b0ce955c575bb41
szhan21 <shunda.zhang@intel.com>
2024-05-19 11:45:56 +0800

    Solving forensics_tangle_heist.

---
+++ BusinessCTF2024/forensics/forensics_tangle_heist/dec.py
@@ -0,0 +1,15 @@
+'''
+
+Copper
+CN=SRV195,OU=Domain Controllers,DC=rebcorp,DC=htb
+rebcorp.htb
+14
+(objectClass=group)
+5
+Radiation
+wWWHomePage
+http://rebcorp.htb/qPvAdQ.php
+B4ck,Enclave
+
+
+'''
