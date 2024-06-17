d47110810c81597690dbdc89ae5c645f4a7e2a06
ShundaZhang <zhangshunda@gmail.com>
2024-06-17 13:32:05 +0800

    Solved aes but twice!

--- VSCTF2024/crypto/aes-but-twice/dec.py
+++ VSCTF2024/crypto/aes-but-twice/dec.py
@@ -7,35 +7,43 @@ from Crypto.Util import Counter
 ip, port = 'vsc.tf', 5000
 io = remote(ip, port)
 
-enc_ctr = bytes.fromhex(io.recvline().strip())
-enc_cbc = bytes.fromhex(io.recvline().strip())
-nonce = bytes.fromhex(io.recvline().strip())
+enc_ctr = bytes.fromhex(io.recvline().decode().strip())
+enc_cbc = bytes.fromhex(io.recvline().decode().strip())
+nonce = bytes.fromhex(io.recvline().decode().strip())
 
 iv = enc_cbc[-16:]
-counter1 = '\x00'*8
-counter2 = '\x00'*7+'\x01'
-counter3 = '\x00'*7+'\x02'
-counter4 = '\x00'*7+'\x03'
+counter1 = b'\x00'*8
+counter2 = b'\x00'*7+b'\x01'
+counter3 = b'\x00'*7+b'\x02'
+counter4 = b'\x00'*7+b'\x03'
 
 p1 = xor((nonce+counter1), iv)
 io.sendline(p1.hex())
-enc_stream1 = bytes.fromhex(io.recvline().strip())[:16]
-iv = bytes.fromhex(io.recvline().strip())[-16:]
+io.recvline()
+enc = bytes.fromhex(io.recvline().decode().strip())
+enc_stream1 = enc[:16]
+iv = enc[-16:]
 
 p1 = xor((nonce+counter2), iv)
 io.sendline(p1.hex())
-enc_stream2 = bytes.fromhex(io.recvline().strip())[:16]
-iv = bytes.fromhex(io.recvline().strip())[-16:]
+io.recvline()
+enc = bytes.fromhex(io.recvline().decode().strip())
+enc_stream2 = enc[:16]
+iv = enc[-16:]
 
 p1 = xor((nonce+counter3), iv)
 io.sendline(p1.hex())
-enc_stream3 = bytes.fromhex(io.recvline().strip())[:16]
-iv = bytes.fromhex(io.recvline().strip())[-16:]
+io.recvline()
+enc = bytes.fromhex(io.recvline().decode().strip())
+enc_stream3 = enc[:16]
+iv = enc[-16:]
 
 p1 = xor((nonce+counter4), iv)
 io.sendline(p1.hex())
-enc_stream4 = bytes.fromhex(io.recvline().strip())[:16]
-iv = bytes.fromhex(io.recvline().strip())[-16:]
+io.recvline()
+enc = bytes.fromhex(io.recvline().decode().strip())
+enc_stream4 = enc[:16]
 
-flag = xor(enc_str, enc_stream1+enc_stream2+enc_stream3+enc_stream4)
+flag = xor(enc_ctr, enc_stream1+enc_stream2+enc_stream3+enc_stream4)
 print(flag)
+#vsctf{me_wen_cbc_6c855453171638d5}
