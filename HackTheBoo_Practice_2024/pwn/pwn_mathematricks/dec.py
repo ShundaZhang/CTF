c63c7c53033ffad9b8694c2a5332d4671db66e75
ShundaZhang <shunda.zhang@intel.com>
2024-10-26 12:34:26 +0000

    Too easy pwn.

---
+++ HackTheBoo_Practice_2024/pwn/pwn_mathematricks/dec.py
@@ -0,0 +1,40 @@
+'''
+       🎉 ~~ w3lC0m3 2 tH3 M4th3M4tR1kCs c0nt35t ~~ 🎉
+
+                        ■ ■ ■ ■ ■ ■ ■
+                        ■           ■
+                        ■ 1. Play   ■
+                        ■ 2. Rules  ■
+                        ■           ■
+                        ■ ■ ■ ■ ■ ■ ■
+
+                        🥸 1
+
+                🎉 ~~ Let the game begin! ~~ 🎉
+
+                Q1: 1 + 1 = ?
+
+                > 2
+
+                [+] THAT WAS AMAZING!
+
+                Q2: 2 - 1 = ?
+
+                > 1
+
+                [+] WE HAVE A MATHEMATICIAN AMONG US!
+
+                Q3: 1337 - 1337 = ?
+
+                > 0
+
+                [+] GOD OF MATHS JUST ENTERED THE CHAT..
+
+                Q4: Enter 2 numbers n1, n2 where n1 > 0 and n2 > 0 and n1 + n2 < 0
+
+                n1: 4294967295
+
+                n2: 4294967295
+HTB{m4th3m4tINT_5tuff_307e308647b5157b4ac831a563c782dc}
+
+'''
---
+++ HackTheBoo_Practice_2024/pwn/pwn_que_onda/challenge/dec.py
@@ -0,0 +1,12 @@
+'''
+▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧
+▧                                              ▧
+▧  Hola mi Amigos! Send me the string "flag"   ▧
+▧  and I will give you uno grande prize!!      ▧
+▧                                              ▧
+▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧▧
+
+$ flag
+HTB{w3lc0m3_2_pwn_f35t1v4l_1cf2063067a699cd55fc35ba1a49ab3c}
+
+'''
