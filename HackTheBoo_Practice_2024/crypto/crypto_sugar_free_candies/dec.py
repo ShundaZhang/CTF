eb373ab7a1af831d18b5981ad15ec96589ad4cf3
ShundaZhang <shunda.zhang@intel.com>
2024-10-26 10:01:52 +0000

    Solved sugar free candies!

---
+++ HackTheBoo_Practice_2024/crypto/crypto_sugar_free_candies/dec.py
@@ -0,0 +1,9 @@
+x1 = 1612993708938936929835517754497931126786454632 
+x2 = 2260690199455691264676123410341531247524997487 
+x3 = 2463132351713367737738737327711828586109296509
+from Crypto.Util.number import long_to_bytes
+print(long_to_bytes(x1))
+print(long_to_bytes(x2))
+print(long_to_bytes(x3))
+
+#HTB{__protecting_the_secret_in_equations_is_not_secure__}
